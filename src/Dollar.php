cae818be338023adbea7e91bb2aa8e0eebfbd918
sho33y <yagishita@buddying.jp>
2018-01-14 12:28:48 +0900

    * 2 = 0 を解決

--- src/Dollar.php
+++ src/Dollar.php
@@ -8,13 +8,20 @@
 
 namespace App;
 
+//TODO リスト
+//・$5 + 10 CHF = $10 (レートが2:1の場合)
+//・$5 * 2 = $10
+//・amount を private にする
+//・Dollarの副作用をどうする？
+//・Money の丸め処理をどうする？
+
 /**
  * Class Dollar
  * @package App
  */
 class Dollar
 {
-    public $amount = 10;
+    public $amount;
 
     /**
      * Dollar constructor.
@@ -22,9 +29,11 @@ class Dollar
      */
     function __construct(int $amount)
     {
+        $this->amount = $amount;
     }
 
     function times(int $multiplier)
     {
+        $this->amount *= $multiplier;
     }
 }
