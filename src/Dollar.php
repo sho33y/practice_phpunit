fdd565852c55fbb9458abcb6d56665ecf66c21c7
sho33y <yagishita@buddying.jp>
2018-01-14 12:56:17 +0900

    TODOリストをwipで管理

--- src/Dollar.php
+++ src/Dollar.php
@@ -8,13 +8,6 @@
 
 namespace App;
 
-//TODO リスト
-//・$5 + 10 CHF = $10 (レートが2:1の場合)
-//・$5 * 2 = $10
-//・amount を private にする
-//・Dollarの副作用をどうする？
-//・Money の丸め処理をどうする？
-
 /**
  * Class Dollar
  * @package App
