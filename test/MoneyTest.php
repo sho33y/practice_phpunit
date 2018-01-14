5d097bf7787d7117dcd74c4bf19d36d4b83981ed
sho33y <yagishita@buddying.jp>
2018-01-14 15:23:23 +0900

    テストの意図がわかりやすいようにtestMultiplicationをリファクタリング

--- test/MoneyTest.php
+++ test/MoneyTest.php
@@ -12,10 +12,8 @@ class MoneyTest extends TestCase
     public function testMultiplication()
     {
         $five = new Dollar(5);
-        $product = $five->times(2);
-        $this->assertEquals(10, $product->amount);
-        $product = $five->times(3);
-        $this->assertEquals(15, $product->amount);
+        $this->assertEquals(new Dollar(10), $five->times(2));
+        $this->assertEquals(new Dollar(15), $five->times(3));
     }
 
     public function testEquality()
