d393826e2a1b7d4b9340850f7850fbcfb1c9241d
sho33y <yagishita@buddying.jp>
2018-01-14 12:02:11 +0900

    Dollar クラス作成

---
+++ src/Dollar.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yagishitasho
+ * Date: 2018/01/14
+ * Time: 11:53
+ */
+
+namespace App;
+
+class Dollar
+{
+    private $amount;
+
+    function __construct(int $amount)
+    {
+
+    }
+
+    function times(int $multiplier)
+    {
+
+    }
+}
