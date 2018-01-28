c4d69f5f78529989dc5d66b1ea908163df5e2d80
sho33y <yagishita@buddying.jp>
2018-01-28 20:44:53 +0900

    足し算のテストを為替レートを適用したテストに変更した

--- test/MoneyTest.php
+++ test/MoneyTest.php
@@ -40,7 +40,10 @@ class MoneyTest extends TestCase
      */
     public function testSimpleAddition()
     {
-        $sum = Money::dollar(5)->plus(Money::dollar(5));
-        $this->assertEquals(Money::dollar(10), $sum);
+        $five = Money::dollar(5);
+        $sum = $five->plus($five);
+        $bank = new Bank();
+        $reduced = $bank->reduce($sum, "USD");
+        $this->assertEquals(Money::dollar(10), $reduce);
     }
 }
