3ca061e15ebd54cb8bf1434f30b63623c5d6d7c2
sho33y <yagishita@buddying.jp>
2018-01-28 12:53:35 +0900

    times内のファクトリメソッドをインライン化した

--- src/Franc.php
+++ src/Franc.php
@@ -30,6 +30,6 @@ class Franc extends Money
      */
     public function times(int $multiplier) : Money
     {
-        return Money::franc($this->amount * $multiplier);
+        return new Franc($this->amount * $multiplier, $this->currency);
     }
 }
