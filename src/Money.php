4cfd499b403ae1234bdcc109f7678803516511f7
sho33y <yagishita@buddying.jp>
2018-01-28 16:38:00 +0900

    ファクトリメソッドでサブクラスから親クラスを参照するように変更した

--- src/Money.php
+++ src/Money.php
@@ -68,7 +68,7 @@ class Money
      */
     public static function dollar(int $amount) : Money
     {
-        return new Dollar($amount, "USD");
+        return new Money($amount, "USD");
     }
 
     /**
@@ -77,6 +77,6 @@ class Money
      */
     public static function franc(int $amount) : Money
     {
-        return new Franc($amount, "CHF");
+        return new Money($amount, "CHF");
     }
 }
