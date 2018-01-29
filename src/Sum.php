4538f0b4cb08ce626bdd581b2198f4acd9bde252
sho33y <yagishita@buddying.jp>
2018-01-29 22:13:39 +0900

    Sumのコンストラクタで引数をフィールドに入れるようにした

--- src/Sum.php
+++ src/Sum.php
@@ -28,6 +28,7 @@ class Sum implements Expression
      */
     public function __construct(Money $augend, Money $addend)
     {
-
+        $this->augend = $augend;
+        $this->addend = $addend;
     }
 }
