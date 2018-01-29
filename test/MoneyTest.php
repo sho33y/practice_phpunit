4ac3695edc9546d5dc599f81996b7e43ee0345a1
sho33y <yagishita@buddying.jp>
2018-01-29 22:19:35 +0900

    reduceメソッドの戻り値が、足し算の結果の金額を持ったMoneyだと確認するテストを追加した

--- test/MoneyTest.php
+++ test/MoneyTest.php
@@ -60,4 +60,15 @@ class MoneyTest extends TestCase
         $this->assertEquals($five, $sum->augend);
         $this->assertEquals($five, $sum->addend);
     }
+
+    /**
+     * @test
+     */
+    public function testReduceSum()
+    {
+        $sum = new Sum(Money::dollar(3), Money::dollar(4));
+        $bank = new Bank();
+        $result = $bank->reduce($sum, "USD");
+        $this->assertEquals(Money::dollar(7), $result);
+    }
 }
