e6e1b3b5d45c7b654512a916cc474ed9d98627eb
sho33y <yagishita@buddying.jp>
2018-01-29 22:38:49 +0900

    Bank.reduce(Money)のテストを追加した

--- test/MoneyTest.php
+++ test/MoneyTest.php
@@ -71,4 +71,14 @@ class MoneyTest extends TestCase
         $result = $bank->reduce($sum, "USD");
         $this->assertEquals(Money::dollar(7), $result);
     }
+
+    /**
+     * @test
+     */
+    public function testReduceMoney()
+    {
+        $bank = new Bank();
+        $result = $bank->reduce(Money::dollar(1), "USD");
+        $this->assertEquals(Money::dollar(1), $result);
+    }
 }
