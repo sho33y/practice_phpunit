1528501b1349f46ae5b85bc2f8e06c6c49da6cd4
sho33y <yagishita@buddying.jp>
2018-01-31 21:50:37 +0900

    為替レートを聞かれた時に返すメソッドを追加した

--- src/Bank.php
+++ src/Bank.php
@@ -50,6 +50,6 @@ class Bank
      */
     public function rate(string $from, string $to) : int
     {
-        return ($from == "CHF" && $to == "USD") ? 2 : 1;
+        return $this->rates[$from . '_' . $to];
     }
 }
